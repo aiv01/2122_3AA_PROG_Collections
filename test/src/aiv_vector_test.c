107fd97927f9020c069bcdabf963de0f8ae05a57
Federico De Felici <fdf8210@gmail.com>
2022-01-25 17:09:39 +0100

    refactored test with setup and teardown

--- test/src/aiv_vector_test.c
+++ test/src/aiv_vector_test.c
@@ -2,34 +2,33 @@
 #include "clove-experimental.h"
 #include "aiv_vector.h"
 
-CLOVE_TEST(CreateVector) {
+ aiv_vector_t *vector;
 
-    aiv_vector_t *vector = aiv_vector_new();
-    CLOVE_NOT_NULL(vector);
+CLOVE_SUITE_SETUP() {
+    vector = aiv_vector_new();
+}
+
+CLOVE_SUITE_TEARDOWN() {
     aiv_vector_free(vector);
+}
+
+CLOVE_TEST(CreateVector) {
+    CLOVE_NOT_NULL(vector);
 }
 
 CLOVE_TEST(CheckIfVectorIsEmptyAfterCreation) {
-
-    aiv_vector_t *vector = aiv_vector_new();
     CLOVE_IS_TRUE(aiv_vector_is_empty(vector));
-    aiv_vector_free(vector);
 }
 
 CLOVE_TEST(AppendOneElement) {
-
-    aiv_vector_t *vector = aiv_vector_new();
     int a = 5;
     aiv_vector_append(vector, &a);
+
     CLOVE_IS_FALSE(aiv_vector_is_empty(vector));
     CLOVE_PTR_EQ(&a, *vector->items);
-    
-    aiv_vector_free(vector);
 }
 
 CLOVE_TEST(AppendTwoElements){
-
-    aiv_vector_t *vector = aiv_vector_new();
     int a = 5;
     int b = 15;
     aiv_vector_append(vector, &a);
@@ -37,29 +36,21 @@ CLOVE_TEST(AppendTwoElements){
 
     CLOVE_PTR_EQ(&a, vector->items[0]);
     CLOVE_PTR_EQ(&b, vector->items[1]);
-
-    aiv_vector_free(vector);
 }
 
 CLOVE_TEST(GetFirstElement){
-    aiv_vector_t *vector = aiv_vector_new();
     int a = 5;
     aiv_vector_append(vector, &a);
 
     CLOVE_PTR_EQ(&a, aiv_vector_at(vector, 0));
-
-    aiv_vector_free(vector);
 }
 
 CLOVE_TEST(GetElementOutOfRange){
-    aiv_vector_t *vector = aiv_vector_new();
     int a = 5;
     aiv_vector_append(vector, &a);
 
     CLOVE_NULL(aiv_vector_at(vector, -1));
     CLOVE_NULL(aiv_vector_at(vector, 2));
-
-    aiv_vector_free(vector);
 }
 
 /*
